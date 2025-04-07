2af2d5de4b12119c0c6283a4c60c02768aa55943
Ricardo Alberto Villarreal Zuñiga <ricardo.leobardin@outlook.com>
2025-04-07 15:41:29 -0600

    Clase finalizada

--- demo.py
+++ demo.py
@@ -1,2 +1,11 @@
-class Cuadrado:
-    pass
+class cuadrado:
+    def __init__(self,lado):
+        self.lado= lado
+
+def area(self):
+    area= self.lado * self.lado
+    return area
+    
+lado=int(input("Ingrese el lado del cuadrado: "))
+print(f"El area del cuadrado es {area}")
+area()
